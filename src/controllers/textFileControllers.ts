ff5bab941c5380891fe371657b2978c2b16d03ab
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-01-14 22:17:14 +0530

    test: test cases added for uploadFileController

--- src/controllers/textFileControllers.ts
+++ src/controllers/textFileControllers.ts
@@ -5,10 +5,10 @@ import FileRepository from '../db/repositories/filesRepository';
 import TaskRepository from '../db/repositories/tasksRepository';
 import { countUniqueWords, countWords, findTopKWords } from '../utils';
 
-const OPTION_ACTION_MAP:any={
-    "countWords":countWords,
-    "countUniqueWords":countUniqueWords,
-    "findTopKWords":findTopKWords
+const OPTION_ACTION_MAP: any = {
+    "countWords": countWords,
+    "countUniqueWords": countUniqueWords,
+    "findTopKWords": findTopKWords
 }
 
 
@@ -18,18 +18,18 @@ const uploadFileController = async (req: Request, res: Response) => {
             return res.status(400).json({ error: 'No file uploaded' });
         }
         const fileName = req.file.originalname;
-        const filePath=req.file.path;
-        
+        const filePath = req.file.path;
+
         const fileRepository = new FileRepository({ dataSource: AppDataSource })
-    
+
         const filePayload: FilePayload = {
             fileName: fileName,
             filePath: filePath
         }
         const fileResponse = await fileRepository.saveFile(filePayload)
-        return res.json({id:fileResponse.raw[0].id})
+        return res.status(200).json({ id: fileResponse.raw[0].id })
     } catch (error) {
-        return res.status(500).json({ error: error||'Internal Server Error' });
+        return res.status(500).json({ error: error || 'Internal Server Error' });
     }
 }
 
@@ -38,46 +38,46 @@ const analyzeFileController = async (req: Request, res: Response) => {
         const { fileId } = req.params;
         const { operation, options } = req.body || {};
         const fileRepository = new FileRepository({ dataSource: AppDataSource })
-        const fileFromDb=await fileRepository.getFileById(parseInt(fileId))
-        if(!fileFromDb){
+        const fileFromDb = await fileRepository.getFileById(parseInt(fileId))
+        if (!fileFromDb) {
             return res.status(400).json({ error: 'No file found' });
         }
-        const {filePath}=fileFromDb;
+        const { filePath } = fileFromDb;
         const fs = require('node:fs');
         const data = fs.readFileSync(filePath, 'utf8');
-        const action=OPTION_ACTION_MAP[operation];
+        const action = OPTION_ACTION_MAP[operation];
 
-        if(!action){
-            return res.status(400).json({error:'Pass valid operation'})
+        if (!action) {
+            return res.status(400).json({ error: 'Pass valid operation' })
         }
 
-        const result=action(data,options);
-        if(result.error){
-             return res.status(400).json({error:result.error})
+        const result = action(data, options);
+        if (result.error) {
+            return res.status(400).json({ error: result.error })
         }
         const taskRepository = new TaskRepository({ dataSource: AppDataSource })
 
         const taskResponse = await taskRepository.saveTask({
-            operation:operation,
-            options:options?JSON.stringify(options):"",
-            result:JSON.stringify({result})
+            operation: operation,
+            options: options ? JSON.stringify(options) : "",
+            result: JSON.stringify({ result })
         })
 
-        return res.json({id:taskResponse.raw[0].id})
+        return res.json({ id: taskResponse.raw[0].id })
     } catch (error) {
         console.log(error)
-        res.status(500).json({error:error})
+        res.status(500).json({ error: error })
     }
 }
 
-const fileResulstsController =async (req: Request, res: Response) => {
+const fileResulstsController = async (req: Request, res: Response) => {
     const { taskId } = req.params;
     const taskRepository = new TaskRepository({ dataSource: AppDataSource })
-    const task=await taskRepository.getTaskById(parseInt(taskId))
-    if(!task){
+    const task = await taskRepository.getTaskById(parseInt(taskId))
+    if (!task) {
         return res.status(400).json({ error: 'Task not found' });
     }
-    res.json({result:task})
+    res.json({ result: task })
 }
 
 export const textFileControllers = {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,63 +1,72 @@
 import { PUNCTUATION_REGX, WORD_REGX } from '../constants';
 
-export const removePunctuation=(word:string)=>{
-      // Remove common punctuation characters
-  return word.replace(PUNCTUATION_REGX, '').toLowerCase();
+export const removePunctuation = (word: string) => {
+    // Remove common punctuation characters
+    return word.replace(PUNCTUATION_REGX, '').toLowerCase();
 
 }
 
-export const countWords=(content:string)=>{
-    if(content.trim().length===0){
+export const countWords = (content: string) => {
+    if (content.trim().length === 0) {
         return 0;
     }
-    return  content.split(WORD_REGX).length;
+    return content.split(WORD_REGX).length;
 }
 
-export const countUniqueWords=(content:string)=>{
-    if(content.trim().length===0){
+export const countUniqueWords = (content: string) => {
+    if (content.trim().length === 0) {
         return 0;
     }
     const wordsArray = content.split(WORD_REGX);
     // Use an object to count word occurrences
-    const wordCount:Record<string,number> = {};
+    const wordCount: Record<string, number> = {};
     const nonDuplicateWordsSet = new Set();
 
     wordsArray.forEach(word => {
-      // Remove punctuation and convert to lowercase
-      const cleanedWord = removePunctuation(word).toLowerCase();
+        // Remove punctuation and convert to lowercase
+        const cleanedWord = removePunctuation(word).toLowerCase();
 
-      const count = wordCount[cleanedWord] || 0;
-      wordCount[cleanedWord] = count + 1;
+        const count = wordCount[cleanedWord] || 0;
+        wordCount[cleanedWord] = count + 1;
 
-      if (count === 0) {
-        // Add to the set if it's the first occurrence
-        nonDuplicateWordsSet.add(cleanedWord);
-      } else {
-        // Remove from the set if it's a repeated occurrence
-        nonDuplicateWordsSet.delete(cleanedWord);
-      }
+        if (count === 0) {
+            // Add to the set if it's the first occurrence
+            nonDuplicateWordsSet.add(cleanedWord);
+        } else {
+            // Remove from the set if it's a repeated occurrence
+            nonDuplicateWordsSet.delete(cleanedWord);
+        }
     });
 
     // Convert the Set back to an array (if needed)
     const nonDuplicateWordsArray = Array.from(nonDuplicateWordsSet);
 
-     return nonDuplicateWordsArray.length;
-   
+    return nonDuplicateWordsArray.length;
+
 }
 
-export const findTopKWords=(content:string,options:TopFreqOption)=>{
-
-    if(!options){
-    return ({error:"Please provide valid options"})
+export const findTopKWords = (content: string, options?: TopFreqOption) => {
+    // Check options are there
+    if (!options) {
+        return ({ error: "Please provide valid options" })
     }
+
+    if(!options.k){
+        return ({ error: "Please provide valid frequency count (k)" })
+    }
+
+    // Use an object to count word frequency
     const wordFrequency: Record<string, number> = {};
+
     content.split(/\s+/).forEach((word) => {
         wordFrequency[word] = (wordFrequency[word] || 0) + 1;
-      });
+    });
 
-      const result = Object.entries(wordFrequency)
+    const result = Object.entries(wordFrequency)
         .sort((a, b) => b[1] - a[1])
         .slice(0, options.k)
         .map(([word, frequency]) => ({ word, frequency }));
+
     return result
 }
+ 
